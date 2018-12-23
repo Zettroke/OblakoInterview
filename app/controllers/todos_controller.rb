98b93b3a73a6df00e1fa151b08921ea630a2719a
Zettroke <skorostnoy2000@yandex.ru>
2018-12-23 16:33:46 +0300

    fix. todo isCompleted now set to false on creation

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,7 +5,9 @@ class TodosController < ApplicationController
 
   def create
 
-    Todo.create params.require(:todo).permit(:text, :project_id)
+    todo = Todo.new params.require(:todo).permit(:text, :project_id)
+    todo.isCompleted = false
+    todo.save
     redirect_to root_path
   end
 end
